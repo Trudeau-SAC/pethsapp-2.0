b0a5d754aa5448c11f2056c3c2c316be644f0745
EdZ543 <53500062+EdZ543@users.noreply.github.com>
2023-01-07 18:04:02 -0500

    Add maximize button to Card component

--- components/RectangleCard.js
+++ components/RectangleCard.js
@@ -1,4 +1,10 @@
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableHighlight,
+} from 'react-native';
 import { useTheme } from '@react-navigation/native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useNavigation } from '@react-navigation/native';
@@ -36,42 +42,65 @@ const RectangleCard = (props) => {
       fontWeight: theme.textVariants.heading6.fontWeight,
       fontSize: theme.textVariants.heading6.fontSize,
     },
-    image: {
+    graphic: {
       resizeMode: 'contain',
       height: '100%',
       width: 144,
       position: 'absolute',
       right: 0,
     },
+    icon: {
+      resizeMode: 'contain',
+      width: '100%',
+      height: '100%',
+    },
+    button: {
+      position: 'absolute',
+      width: 36,
+      height: 36,
+      right: 16,
+      top: 16,
+      borderRadius: '50%',
+      backgroundColor: theme.colors.onPrimary,
+      padding: 8,
+    },
   });
 
   return (
-    <TouchableOpacity
+    <View
       style={styles.container}
       onPress={() => navigation.navigate(props.navigateTo)}
       underlayColor={theme.colors.onPrimary}
     >
-      <View style={styles.background}>
-        {props.imageSource && (
-          <Image source={props.imageSource} style={styles.image} />
+      {props.imageSource && (
+        <Image source={props.imageSource} style={styles.graphic} />
+      )}
+      <LinearGradient
+        style={styles.background}
+        colors={[theme.colors.card + 'CC', theme.colors.card + '00']}
+        start={{ x: 0, y: 1 }}
+        end={{ x: 1, y: 0 }}
+      />
+      <View style={styles.textContainer}>
+        {props.supertitle && (
+          <Text style={styles.supertitle}>{props.supertitle}</Text>
         )}
-        <LinearGradient
-          style={styles.background}
-          colors={[theme.colors.card + 'CC', theme.colors.card + '00']}
-          start={{ x: 0, y: 1 }}
-          end={{ x: 1, y: 0 }}
-        />
-        <View style={styles.textContainer}>
-          {props.supertitle && (
-            <Text style={styles.supertitle}>{props.supertitle}</Text>
-          )}
-          {props.title && <Text style={styles.title}>{props.title}</Text>}
-          {props.subtitle && (
-            <Text style={styles.subtitle}>{props.subtitle}</Text>
-          )}
-        </View>
+        {props.title && <Text style={styles.title}>{props.title}</Text>}
+        {props.subtitle && (
+          <Text style={styles.subtitle}>{props.subtitle}</Text>
+        )}
       </View>
-    </TouchableOpacity>
+      <TouchableHighlight
+        style={styles.button}
+        underlayColor={theme.colors.neutral2}
+        onPress={() => navigation.navigate(props.navigateTo)}
+      >
+        <Image
+          style={styles.icon}
+          source={require('../assets/icons/maximize-icon.png')}
+        />
+      </TouchableHighlight>
+    </View>
   );
 };
 
