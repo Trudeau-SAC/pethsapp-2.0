307a2b1a06f1c90c307ae8ce08a857de0911646b
EdZ543 <53500062+EdZ543@users.noreply.github.com>
2023-01-08 10:33:16 -0500

    Style bottom tab bar

--- screens/home/HomeTabs.js
+++ screens/home/HomeTabs.js
@@ -2,6 +2,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useTheme } from '@react-navigation/native';
 import { View, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 import Home from './Home';
 import Community from '../community/Community';
@@ -20,31 +21,55 @@ const HomeTabs = () => {
       borderTopColor: 'transparent',
       height: 100,
     },
-    gradient: {
+    tabBarBackground: {
       height: '100%',
     },
   });
 
   return (
     <Tabs.Navigator
-      screenOptions={{
+      screenOptions={({ route }) => ({
         headerShown: false,
         tabBarShowLabel: false,
         tabBarStyle: styles.tabBar,
         tabBarBackground: () => (
           <View>
             <LinearGradient
-              style={styles.gradient}
+              style={styles.tabBarBackground}
               colors={[
-                theme.colors.primary + 'CC',
+                theme.colors.primary + 'FF',
+                theme.colors.primary + '66',
                 theme.colors.primary + '00',
               ]}
+              locations={[0, 0.7, 1]}
               start={{ x: 0, y: 1 }}
               end={{ x: 0, y: 0 }}
             />
           </View>
         ),
-      }}
+        tabBarActiveTintColor: theme.colors.neutral1,
+        tabBarInactiveTintColor: theme.colors.onPrimary,
+        tabBarIcon: ({ color }) => {
+          let iconName;
+
+          switch (route.name) {
+            case 'Home':
+              iconName = 'home';
+              break;
+            case 'Community':
+              iconName = 'users';
+              break;
+            case 'Resources':
+              iconName = 'book-open';
+              break;
+            case 'Settings':
+              iconName = 'settings';
+              break;
+          }
+
+          return <Feather name={iconName} size={32} color={color} />;
+        },
+      })}
     >
       <Tabs.Screen name="Home" component={Home} />
       <Tabs.Screen name="Community" component={Community} />
