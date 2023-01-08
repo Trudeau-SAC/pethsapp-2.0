e0854af6907f7eb7235ae72b8cdafedecdd32399
EdZ543 <53500062+EdZ543@users.noreply.github.com>
2023-01-08 17:00:45 -0500

    Add tab bar padding

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
-import { StyleSheet, ScrollView, View } from 'react-native';
+import { ScrollView, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { useTheme } from '@react-navigation/native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 /**
  * Provides styles common to all screens
@@ -10,22 +11,35 @@ export default function Layout({ children, scrolling }) {
   const insets = useSafeAreaInsets();
   const theme = useTheme();
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      // Padding is set to the safe area insets to avoid the notch on ios
-      paddingTop: insets.top,
-      paddingBottom: insets.bottom,
-      // Left and right padding
-      paddingLeft: theme.spacing.s5,
-      paddingRight: theme.spacing.s5,
-    },
-  });
+  const content = (
+    <>
+      <LinearGradient
+        style={{
+          height: 162,
+        }}
+        colors={[theme.colors.primary + 'FF', theme.colors.primary + '00']}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 0, y: 1 }}
+      />
+      <View
+        style={{
+          flex: 1,
+          // Padding is set to the safe area insets to avoid the notch on ios
+          paddingTop: insets.top,
+          // Account for tab bar height
+          paddingBottom: theme.tabBarHeight,
+          // Left and right padding
+          paddingLeft: theme.spacing.s5,
+          paddingRight: theme.spacing.s5,
+          position: 'absolute',
+          top: 0,
+        }}
+      >
+        {children}
+      </View>
+    </>
+  );
 
   // Renders a different type of container depending on whether the screen should scroll
-  if (scrolling) {
-    return <ScrollView style={styles.container}>{children}</ScrollView>;
-  } else {
-    return <View style={styles.container}>{children}</View>;
-  }
+  return <ScrollView>{content}</ScrollView>;
 }
