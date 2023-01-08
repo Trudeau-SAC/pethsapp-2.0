772f80df2b4f0285e77290617194d500c4c613ae
EdZ543 <53500062+EdZ543@users.noreply.github.com>
2023-01-08 09:34:17 -0500

    Add gradient to tab bar

--- screens/home/HomeTabs.js
+++ screens/home/HomeTabs.js
@@ -1,4 +1,7 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { LinearGradient } from 'expo-linear-gradient';
+import { useTheme } from '@react-navigation/native';
+import { View, StyleSheet } from 'react-native';
 
 import Home from './Home';
 import Community from '../community/Community';
@@ -8,8 +11,41 @@ import Settings from '../settings/Settings';
 const Tabs = createBottomTabNavigator();
 
 const HomeTabs = () => {
+  const theme = useTheme();
+
+  const styles = StyleSheet.create({
+    tabBar: {
+      position: 'absolute',
+      backgroundColor: 'transparent',
+      borderTopColor: 'transparent',
+      height: 100,
+    },
+    gradient: {
+      height: '100%',
+    },
+  });
+
   return (
-    <Tabs.Navigator screenOptions={{ headerShown: false }}>
+    <Tabs.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: styles.tabBar,
+        tabBarBackground: () => (
+          <View>
+            <LinearGradient
+              style={styles.gradient}
+              colors={[
+                theme.colors.primary + 'CC',
+                theme.colors.primary + '00',
+              ]}
+              start={{ x: 0, y: 1 }}
+              end={{ x: 0, y: 0 }}
+            />
+          </View>
+        ),
+      }}
+    >
       <Tabs.Screen name="Home" component={Home} />
       <Tabs.Screen name="Community" component={Community} />
       <Tabs.Screen name="Resources" component={Resources} />
