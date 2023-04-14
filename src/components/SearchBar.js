3f6e1b685961343bcabdcdc1275f8c39303539de
EdZ543 <53500062+EdZ543@users.noreply.github.com>
2023-04-14 12:38:09 -0400

    Create SearchBar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,40 @@
+import { View, StyleSheet, TextInput } from 'react-native';
+import { useTheme } from '@react-navigation/native';
+import { Feather } from '@expo/vector-icons';
+
+export default function SearchBar({ value, onChangeText, placeholder }) {
+  const theme = useTheme();
+  const styles = StyleSheet.create({
+    container: {
+      backgroundColor: theme.colors.background,
+      borderColor: theme.colors.border,
+      borderWidth: 1,
+      padding: 16,
+      borderRadius: 16,
+      flex: 0,
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    textInput: {
+      ...theme.textVariants.heading7,
+      color: theme.colors.text,
+      height: 22,
+    },
+    icon: {
+      color: theme.colors.text,
+      marginRight: 12,
+    },
+  });
+
+  return (
+    <View style={styles.container}>
+      <Feather style={styles.icon} name="search" size={16} color="black" />
+      <TextInput
+        value={value}
+        style={styles.textInput}
+        placeholder={placeholder}
+        onChangeText={onChangeText}
+      />
+    </View>
+  );
+}
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,6 +2,7 @@ import { View, Button } from 'react-native';
 import Layout from '../components/Layout';
 import HomeHeader from '../components/HomeHeader';
 import { useTheme } from '@react-navigation/native';
+import SearchBar from '../components/SearchBar';
 
 const Home = ({ navigation }) => {
   const theme = useTheme();
@@ -9,6 +10,8 @@ const Home = ({ navigation }) => {
   return (
     <Layout>
       <HomeHeader />
+
+      <SearchBar value="" placeholder="Search for clubs..." />
     </Layout>
   );
 };
