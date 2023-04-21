cc6d898f4a7a17f30e2c699ae616fe5b5a934cf2
EdZ543 <53500062+EdZ543@users.noreply.github.com>
2023-04-20 21:12:23 -0400

    Connect announcements to CMS

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,41 @@
-import { View, Button } from 'react-native';
 import Layout from '../components/Layout';
 import HomeHeader from '../components/HomeHeader';
-import { useTheme } from '@react-navigation/native';
-import SearchBar from '../components/SearchBar';
+import { useEffect, useState } from 'react';
+import RectangleCard from '../components/RectangleCard';
+
+const SANITY_PROJECT_ID = '8if5onrn';
+const SANITY_DATASET = 'production';
+const QUERY = encodeURIComponent('*[_type == "announcement"]');
+const URL = `https://${SANITY_PROJECT_ID}.api.sanity.io/v2023-04-20/data/query/${SANITY_DATASET}?query=${QUERY}`;
 
 const Home = ({ navigation }) => {
-  const theme = useTheme();
+  const [announcements, setAnnouncements] = useState([]);
+
+  useEffect(() => {
+    let ignore = false;
+
+    fetch(URL)
+      .then((res) => res.json())
+      .then(({ result }) => {
+        if (!ignore) {
+          setAnnouncements(result);
+        }
+      })
+      .catch((err) => console.error(err));
+
+    return () => {
+      ignore = true;
+    };
+  }, []);
 
   return (
     <Layout>
       <HomeHeader />
-
-      <SearchBar value="" placeholder="Search for clubs..." />
+      {announcements.map((announcement) => {
+        return (
+          <RectangleCard key={announcement?.date} title={announcement?.date} />
+        );
+      })}
     </Layout>
   );
 };
