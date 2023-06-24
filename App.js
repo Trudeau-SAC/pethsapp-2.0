507d40054b7158f2af6cc8caeeb9b8d0d7b98a6b
EdZ543 <53500062+EdZ543@users.noreply.github.com>
2023-06-24 10:11:35 -0400

    Create first dev build

--- App.js
+++ App.js
@@ -6,8 +6,9 @@ import { lightTheme, darkTheme } from './constants/themes';
 import { registerRootComponent } from 'expo';
 import { SettingsProvider, useSettings } from './contexts/SettingsContext';
 import * as SplashScreen from 'expo-splash-screen';
-import { useCallback } from 'react';
+import { useCallback, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
+import { registerForPushNotificationsAsync } from './lib/notifications';
 
 import HomeTabs from './HomeTabs';
 import Announcement from './screens/home/AnnouncementScreen';
@@ -23,6 +24,13 @@ const Stack = createNativeStackNavigator();
 const App = () => {
   const settings = useSettings();
 
+  useEffect(() => {
+    if (settings['Announcements and Events'] || settings['Snow Day']) {
+      registerForPushNotificationsAsync();
+    }
+  }, [settings]);
+
+  // Load fonts
   const [fontsLoaded] = useFonts({
     GeneralSansBold: require('./assets/fonts/GeneralSans-Bold.otf'),
     GeneralSansBoldItalic: require('./assets/fonts/GeneralSans-BoldItalic.otf'),
--- app.config.js
+++ app.config.js
@@ -1,37 +1,11 @@
-module.exports = {
-  name: 'pethsapp',
-  slug: 'pethsapp',
-  version: '1.0.0',
-  orientation: 'portrait',
-  icon: './assets/icons/icon.png',
-  userInterfaceStyle: 'light',
-  splash: {
-    image: './assets/icons/splash.png',
-    resizeMode: 'contain',
-    backgroundColor: '#ffffff',
-  },
-  updates: {
-    fallbackToCacheTimeout: 0,
-  },
-  assetBundlePatterns: ['**/*'],
-  ios: {
-    supportsTablet: true,
-    config: {
-      usesNonExemptEncryption: false,
+module.exports = ({ config }) => {
+  return {
+    ...config,
+    extra: {
+      ...config.extra,
+      sanityProjectId: process.env.SANITY_PROJECT_ID,
+      sanityDataset: process.env.SANITY_DATASET,
+      sanityApiVersion: process.env.SANITY_API_VERSION,
     },
-  },
-  android: {
-    adaptiveIcon: {
-      foregroundImage: './assets/icons/adaptive-icon.png',
-      backgroundColor: '#FFFFFF',
-    },
-  },
-  web: {
-    favicon: './assets/icons/favicon.png',
-  },
-  extra: {
-    sanityProjectId: process.env.SANITY_PROJECT_ID,
-    sanityDataset: process.env.SANITY_DATASET,
-    sanityApiVersion: process.env.SANITY_API_VERSION,
-  },
+  };
 };
