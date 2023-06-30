b9c64545b4b0447fa53a536ee6eb95590e075754
EdZ543 <53500062+EdZ543@users.noreply.github.com>
2023-06-30 11:57:48 -0400

    Create development build

--- app.config.js
+++ app.config.js
@@ -1,6 +1,14 @@
 module.exports = ({ config }) => {
   return {
     ...config,
+    android: {
+      ...config.android,
+      googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+    },
+    ios: {
+      ...config.ios,
+      googleServicesFile: process.env.GOOGLE_SERVICES_PLIST,
+    },
     extra: {
       ...config.extra,
       sanityProjectId: process.env.SANITY_PROJECT_ID,
