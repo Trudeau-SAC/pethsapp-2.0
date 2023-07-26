73bd94ef35330951c954d5bcb6979a4b59610c0d
EdZ543 <53500062+EdZ543@users.noreply.github.com>
2023-07-26 16:12:54 -0400

    Add margins to rich text h1

--- components/RichText.js
+++ components/RichText.js
@@ -3,48 +3,58 @@ import { fonts } from '../constants/themes';
 import { Image } from 'expo-image';
 import { builder } from '../lib/sanity';
 import { getImageDimensions } from '@sanity/asset-utils';
-
+import { StyleSheet } from 'react-native';
 import Text from './Text';
-
-const components = {
-  types: {
-    image: ({ value }) => {
-      const { width, height } = getImageDimensions(value);
-
-      return (
-        <Image
-          style={{
-            width: '100%',
-            aspectRatio: width / height,
-            borderRadius: 16,
-          }}
-          source={builder.image(value).url()}
-        />
-      );
-    },
-  },
-  block: {
-    h1: ({ children }) => (
-      <Text color="text" variant="heading5">
-        {children}
-      </Text>
-    ),
-    normal: ({ children }) => (
-      <Text color="text" variant="body1">
-        {children}
-      </Text>
-    ),
-  },
-  marks: {
-    strong: ({ children }) => (
-      <Text style={{ fontFamily: fonts.bold }}>{children}</Text>
-    ),
-    em: ({ children }) => (
-      <Text style={{ fontFamily: fonts.italic }}>{children}</Text>
-    ),
-  },
-};
+import { useTheme } from '@react-navigation/native';
 
 export default function RichText({ value }) {
+  const theme = useTheme();
+
+  const styles = StyleSheet.create({
+    h1: {
+      marginTop: theme.spacing.s5,
+      marginBottom: theme.spacing.s3,
+    },
+  });
+
+  const components = {
+    types: {
+      image: ({ value }) => {
+        const { width, height } = getImageDimensions(value);
+
+        return (
+          <Image
+            style={{
+              width: '100%',
+              aspectRatio: width / height,
+              borderRadius: 16,
+            }}
+            source={builder.image(value).url()}
+          />
+        );
+      },
+    },
+    block: {
+      h1: ({ children }) => (
+        <Text color="text" variant="heading5" style={styles.h1}>
+          {children}
+        </Text>
+      ),
+      normal: ({ children }) => (
+        <Text color="text" variant="body1">
+          {children}
+        </Text>
+      ),
+    },
+    marks: {
+      strong: ({ children }) => (
+        <Text style={{ fontFamily: fonts.bold }}>{children}</Text>
+      ),
+      em: ({ children }) => (
+        <Text style={{ fontFamily: fonts.italic }}>{children}</Text>
+      ),
+    },
+  };
+
   return <PortableText value={value} components={components} />;
 }
